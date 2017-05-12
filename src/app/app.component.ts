1f063148f491c8fe2e081ebe27116d6119eb2476
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-05-12 10:25:12 +0700

    tambah konfigurasi angular bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Selamat Datang';
+  description = 'Untuk Dapat Mengakses Lagu, silahkan sign in terlebih dahulu :)'
+  clientId = '3926f1313b24408a942e4cd64a7b74cd'
+  responseType = 'code'
+  redirectUri = 'http%3A%2F%2Flocalhost%3A4200%2Fcallback'
+  scope = 'user-read-email user-read-birthdate playlist-modify-public playlist-read-private'
 }
