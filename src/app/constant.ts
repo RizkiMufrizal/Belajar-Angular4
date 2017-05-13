151d79316b23c90db43cfcef99a99986f4c3c507
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-05-13 11:43:57 +0700

    tambah konfigurasi deploy ke heroku

---
+++ src/app/constant.ts
@@ -0,0 +1,4 @@
+export const clientId = '3926f1313b24408a942e4cd64a7b74cd'
+export const responseType = 'token'
+export const redirectUri = 'https://belajar-angular4.herokuapp.com/callback'
+export const scope = 'user-read-private user-read-email user-read-birthdate playlist-modify-public playlist-read-private'
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { clientId, responseType, redirectUri, scope } from './../constant';
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,8 +10,8 @@ export class HomeComponent {
 
   title = 'Selamat Datang';
   description = 'Untuk Dapat Mengakses Lagu, silahkan sign in terlebih dahulu :)'
-  clientId = '3926f1313b24408a942e4cd64a7b74cd'
-  responseType = 'token'
-  redirectUri = 'http%3A%2F%2Flocalhost%3A4200%2Fcallback'
-  scope = 'user-read-private user-read-email user-read-birthdate playlist-modify-public playlist-read-private'
+  clientId = clientId
+  responseType = responseType
+  redirectUri = redirectUri
+  scope = scope
 }
